0dd5a459384a40e445d5bd64df996103ec3769ab
Uli Köhler <ukoehler@techoverflow.net>
2021-12-11 02:59:10 +0100

    Bump version to 1.2

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -3,8 +3,8 @@
 # Call from the repository root: ./scripts/build-docker.sh
 
 # Build image
-docker build -t ulikoehler/flaredns:latest -t ulikoehler/flaredns:1 -t ulikoehler/flaredns:1.1 .
+docker build -t ulikoehler/flaredns:latest -t ulikoehler/flaredns:1 -t ulikoehler/flaredns:1.2 .
 # Push image
 docker push ulikoehler/flaredns:latest
 docker push ulikoehler/flaredns:1
-docker push ulikoehler/flaredns:1.1
+docker push ulikoehler/flaredns:1.2
